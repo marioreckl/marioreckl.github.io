111c812cbfe1fe800ede1ce45a0850b6f2b516c3
Mario Reckl <marioreckl@hotmail.com>
2019-01-05 12:16:58 -0500

    Added Gamenight page and fixed console errors

--- libraries/scrolling.js
+++ libraries/scrolling.js
@@ -1,4 +1,4 @@
-﻿
+
 $(function () {
     $(window).on("load resize", function () {
         $(".fill-screen").css("height", window.innerHeight);
@@ -13,7 +13,7 @@ $(function () {
     // smooth scrolling
     $('nav a, .down-button a').bind('click', function () {
         $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top + 1
+            scrollTop: $($(this).attr('href')).offset().top 
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
